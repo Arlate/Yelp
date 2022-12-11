e2a565b0c3a8f5a6bb72fbecb1fc18de9fa3875d
Arlate <117584094+Arlate@users.noreply.github.com>
2022-12-11 18:08:08 +0100

    Update showMap.js

--- public/showMap.js
+++ public/showMap.js
@@ -1,4 +1,6 @@
-mapboxgl.accessToken = '';
+import * as dotenv from 'dotenv'
+dotenv.config()
+mapboxgl.accessToken = process.env.MAP_TOKEN;
 const map = new mapboxgl.Map({
     container: 'map', // container ID
     style: 'mapbox://styles/mapbox/outdoors-v10', // style URL
